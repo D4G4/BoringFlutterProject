af4a738f6d55a4afd4bd369cd8561d2a027f7ada
daksh.gargas <daksh.gargas@peoplestrong.com>
2018-08-20 16:44:13 +0530

    Completed tutorial 2 Networking and Future + source_gen

--- test/json_test.dart
+++ test/json_test.dart
@@ -1,6 +1,7 @@
 import 'package:test/test.dart';
-import 'package:boring_flutter_project/json_parsing.dart';
-
+import 'package:boring_flutter_project/source_gen_code__parsers/json_parsing.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   test("parses topstories.json", () {
@@ -12,6 +13,19 @@ void main() {
   test("parses item.json", () {
     const jsonString =
         """{"by":"dhouston","descendants":71,"id":8863,"kids":[9224,8952,8917,8884,8887,8943,8869,8940,8908,8958,9005,8873,9671,9067,9055,8865,8881,8872,8955,10403,8903,8928,9125,8998,8901,8902,8907,8894,8870,8878,8980,8934,8876],"score":104,"time":1175714200,"title":"My YC app: Dropbox - Throw away your USB drive","type":"story","url":"http://www.getdropbox.com/u/2/screencast.html"}""";
-    expect(parseArticle(jsonString).by, "dhouston");    
+    expect(parseArticle(jsonString).by, "dhouston");
+  });
+
+  test("parse item.json over network", () async {
+    final url = 'https://hacker-news.firebaseio.com/v0/beststories.json';
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      List<dynamic> ids = jsonDecode(response.body);
+      final url2 = 'https://hacker-news.firebaseio.com/v0/item/${ids[3]}.json';
+      final response2 = await http.get(url2);
+      if (response2.statusCode == 200) {
+        expect(parseArticle(response2.body), isNotNull);
+      }
+    }
   });
 }
